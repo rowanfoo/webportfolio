9daa0990f5fa5b7e9255026024d00e5d446d92b3
rowanfoo <rowanfoo@gmail.com>
2021-09-23 21:33:00 +0800

    fix bug

--- src/share/Config.ts
+++ src/share/Config.ts
@@ -1,6 +1,6 @@
 export class Config {
 //    static rooturl = 'http://rowanfoo.ddns.net';
-  static rooturl = 'http://localhost:10150';
+  static rooturl = 'http://rowanfoo.ddns.net:10150';
 //  static rooturl = 'http://192.168.0.10';
 
 //  static trackerurl = 'http://localhost:8080/tracker'
